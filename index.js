88d39de140f5a114bb1796f22821139a31597878
vishwajeet singh <vsrnitp@gmail.com>
2021-01-24 15:55:58 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const ejs = require('ejs');
+
+
+const app = express();
+
+//setting up the view engine....
+app.set("view engine","ejs");
+
+//telling express where are we keeping our index.js...
+app.set("views",__dirname+"/views");
+
+//setting up static folder...
+app.use( express.static( "public" ) );
+
+
+app.get('/',(req,res)=>{
+    res.render("index");
+})
+
+
+const port = 8080||process.env.port;
+app.listen(port,(err,resp)=>{
+    if(err)console.log(err);
+    else{
+        console.log('App woking fine');
+    }
+})
