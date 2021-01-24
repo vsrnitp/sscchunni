9da07d0f9ac4faf824fe5db1ff0e7235b269a92d
vishwajeet singh <vsrnitp@gmail.com>
2021-01-24 19:21:53 +0530

    v 3.1.0

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.get('/',(req,res)=>{
 })
 
 
-const port = process.env.PORT;
+//const port = process.env.PORT;
+const port = 8080;
 app.listen(port,(err,resp)=>{
     if(err)console.log(err);
     else{
